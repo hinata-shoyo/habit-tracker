fb6748b8f2b750cc5f29860c44e2973c128d849f
hinata-shoyo <avsharma4500@gmail.com>
2025-02-12 21:28:59 +0530

    fix

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { getSession } from "@/lib/auth";
 
 export default async function Dashboard() {
   const session = await getSession();
-  if (!session?.user.id) {
+  if (!session?.user) {
     return <h1>Please log in... </h1>;
   }
   return (
