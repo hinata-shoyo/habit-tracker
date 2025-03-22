139fda6790f02813145b53ab1dcd70a7fab97648
hinata-shoyo <avsharma4500@gmail.com>
2025-03-22 14:49:42 +0530

    added manifest.ts

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { Sun, Moon, LogOut } from "lucide-react";
 import { signIn, signOut, useSession } from "next-auth/react";
-import {  useContext } from "react";
+import { useContext } from "react";
 import { themeContext } from "./providers";
+import Image from "next/image";
 
 export default function Appbar() {
   const session = useSession();
@@ -10,9 +11,25 @@ export default function Appbar() {
 
   return (
     <div className="fixed backdrop-blur-xl  shadow-lg top-0 w-full h-24  border-b-[0.5px] dark:border-gray-600 border-black border-opacity-50 dark:text-white">
-      <div className="flex justify-between mx-12 lg:mx-96 items-center h-full ">
+      <div className="flex justify-between mx-6 lg:mx-96 items-center h-full ">
         <div className="flex">
-          <div className="font-extrabold text-xl">HabitTracker</div>
+          <div className="font-extrabold text-xl">
+            {useTheme?.theme === "dark" ? (
+              <Image
+                src="/habitualLogoDark.png"
+                alt=""
+                width={100}
+                height={100}
+              />
+            ) : (
+              <Image
+                src="/habitualLogoLight.png"
+                alt=""
+                width={100}
+                height={100}
+              />
+            )}
+          </div>
         </div>
         <div className="flex h-full items-center gap-8">
           <button onClick={useTheme?.toggleTheme}>
@@ -23,13 +40,13 @@ export default function Appbar() {
               <div>
                 <button
                   className="h-10 bg-violet-600 hidden lg:block w-28 rounded-md border-[1px] border-violet-500 text-violet-500 dark:text-white"
-                  onClick={() => signOut({ callbackUrl: "/", })}
+                  onClick={() => signOut({ callbackUrl: "/" })}
                 >
                   Logout
                 </button>
                 <button
                   className="block lg:hidden cursor-pointer"
-                  onClick={() => signOut({callbackUrl:"/"})}
+                  onClick={() => signOut({ callbackUrl: "/" })}
                 >
                   <LogOut
                     className="block lg:hidden cursor-pointer"
@@ -40,7 +57,7 @@ export default function Appbar() {
             )}
             {!session.data?.user && (
               <button
-                  className="h-10 bg-violet-600 w-28 rounded-md border-[1px] border-violet-500 text-violet-500 dark:text-white"
+                className="h-10 bg-violet-600 w-28 rounded-md border-[1px] border-violet-500 text-white"
                 onClick={() => signIn()}
               >
                 Login
